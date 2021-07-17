edecc3bb175810f312fa62a57001cab3db21985a
Julio Zittei <julio.zittei96@gmail.com>
2021-07-17 18:47:16 -0300

    Create Component GitKutCommoms.

---
+++ src/lib/GitKutCommoms.tsx
@@ -0,0 +1,532 @@
+import React from "react"
+import styled, { css } from "styled-components"
+import NextLink from "next/link"
+
+function Link({ href, children, ...props }) {
+  return (
+    <NextLink href={href} passHref>
+      <a {...props}>{children}</a>
+    </NextLink>
+  )
+}
+
+// ================================================================================================================
+// Menu src="./images/logo.png" rc={`${BASE_URL}/logo.svg`}
+// ================================================================================================================
+export function GitkutMenu({ githubUser }) {
+  const [isMenuOpen, setMenuState] = React.useState(false)
+  return (
+    <GitkutMenu.Wrapper isMenuOpen={isMenuOpen}>
+      <div className="container">
+        <GitkutMenu.Logo src="images/logo.png" alt="Logo do Gitkut" />
+
+        <nav style={{ flex: 1 }}>
+          {[
+            { name: "Inicio", slug: "/" },
+            { name: "Amigos", slug: "/amigos" },
+            { name: "Comunidades", slug: "/comunidades" },
+          ].map((menuItem) => (
+            <Link
+              key={`key__${menuItem.name.toLocaleLowerCase()}`}
+              href={`${menuItem.slug.toLocaleLowerCase()}`}
+            >
+              {menuItem.name}
+            </Link>
+          ))}
+        </nav>
+
+        <nav>
+          <a href={`/logout`}>Sair</a>
+          <div>
+            <input placeholder="Pesquisar no Gitkut" />
+          </div>
+        </nav>
+
+        <button onClick={() => setMenuState(!isMenuOpen)}>
+          {isMenuOpen && <img src="icons/menu-open.svg" />}
+          {!isMenuOpen && <img src="icons/menu-closed.svg" />}
+        </button>
+      </div>
+      <GitkutMenuProfileSidebar githubUser={githubUser} />
+    </GitkutMenu.Wrapper>
+  )
+}
+GitkutMenu.Wrapper = styled.header<{
+  isMenuOpen: boolean
+}>`
+  width: 100%;
+  background-color: #308bc5;
+
+  .GitkutMenuProfileSidebar {
+    background: white;
+    position: fixed;
+    z-index: 100;
+    padding: 46px;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 48px;
+    transition: 0.3s;
+    pointer-events: ${({ isMenuOpen }) => (isMenuOpen ? "all" : "none")};
+    opacity: ${({ isMenuOpen }) => (isMenuOpen ? "1" : "0")};
+    transform: ${({ isMenuOpen }) =>
+      isMenuOpen ? "translateY(0)" : "translateY(calc(-100% - 48px))"};
+    @media (min-width: 860px) {
+      display: none;
+    }
+    > div {
+      max-width: 400px;
+      margin: auto;
+    }
+    a {
+      font-size: 18px;
+    }
+    .boxLink {
+      font-size: 18px;
+      color: #2e7bb4;
+      -webkit-text-decoration: none;
+      text-decoration: none;
+      font-weight: 800;
+    }
+
+    hr {
+      margin-top: 12px;
+      margin-bottom: 8px;
+      border-color: transparent;
+      border-bottom-color: #ecf2fa;
+    }
+  }
+
+  .container {
+    background-color: #308bc5;
+    padding: 7px 16px;
+    max-width: 1110px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    z-index: 101;
+    @media (min-width: 860px) {
+      justify-content: flex-start;
+    }
+
+    button {
+      border: 0;
+      background: transparent;
+      align-self: center;
+      display: inline-block;
+      @media (min-width: 860px) {
+        display: none;
+      }
+    }
+
+    nav {
+      display: none;
+      @media (min-width: 860px) {
+        display: flex;
+      }
+      a {
+        font-size: 12px;
+        color: white;
+        padding: 10px 16px;
+        position: relative;
+        text-decoration: none;
+        &:after {
+          content: " ";
+          background-color: #5292c1;
+          display: block;
+          position: absolute;
+          width: 1px;
+          height: 12px;
+          margin: auto;
+          left: 0;
+          top: 0;
+          bottom: 0;
+        }
+      }
+    }
+    input {
+      color: #ffffff;
+      background: #5579a1;
+      padding: 10px 42px;
+      border: 0;
+      background-image: url("icons/search.svg");
+      background-position: 15px center;
+      background-repeat: no-repeat;
+      border-radius: 1000px;
+      font-size: 12px;
+      ::placeholder {
+        color: #ffffff;
+        opacity: 1;
+      }
+    }
+  }
+`
+GitkutMenu.Logo = styled.img`
+  background-color: #ffffff;
+  padding: 9px 16px;
+  border-radius: 1000px;
+  height: 34px;
+`
+
+function GitkutMenuProfileSidebar({ githubUser }) {
+  return (
+    <div className="GitkutMenuProfileSidebar">
+      <div>
+        <img
+          src={`https://github.com/${githubUser}.png`}
+          style={{ borderRadius: "8px" }}
+        />
+        <hr />
+        <p>
+          <a className="boxLink" href={`/user/${githubUser}`}>
+            @{githubUser}
+          </a>
+        </p>
+        <hr />
+
+        <GitkutProfileSidebarMenuDefault />
+      </div>
+    </div>
+  )
+}
+
+// ================================================================================================================
+// GitkutProfileSidebarMenuDefault
+// ================================================================================================================
+export function GitkutProfileSidebarMenuDefault() {
+  return (
+    <GitkutProfileSidebarMenuDefault.Wrapper>
+      <nav>
+        <a href="/">
+          <img src="icons/user.svg" />
+          Perfil
+        </a>
+        <a href="/">
+          <img src="icons/book.svg" />
+          Recados
+        </a>
+        <a href="/">
+          <img src="icons/camera.svg" />
+          Fotos
+        </a>
+        <a href="/">
+          <img src="icons/sun.svg" />
+          Depoimentos
+        </a>
+      </nav>
+      <hr />
+      <nav>
+        <a href="/">
+          <img src="icons/plus.svg" />
+          GitHub Trends
+        </a>
+        <a href="/logout">
+          <img src="icons/logout.svg" />
+          Sair
+        </a>
+      </nav>
+    </GitkutProfileSidebarMenuDefault.Wrapper>
+  )
+}
+GitkutProfileSidebarMenuDefault.Wrapper = styled.div`
+  a {
+    font-size: 12px;
+    color: #2e7bb4;
+    margin-bottom: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    text-decoration: none;
+    img {
+      width: 16px;
+      height: 16px;
+      margin-right: 5px;
+    }
+  }
+`
+
+// ================================================================================================================
+// OrkutNostalgicIconSet
+// ================================================================================================================
+export function OrkutNostalgicIconSet(props) {
+  return (
+    <OrkutNostalgicIconSet.List>
+      {[
+        { name: "Recados", slug: "recados", icon: "book" },
+        { name: "Fotos", slug: "fotos", icon: "camera" },
+        { name: "Videos", slug: "videos", icon: "video-camera" },
+        { name: "Fãs", slug: "fas", icon: "star" },
+        { name: "Mensagens", slug: "mensagens", icon: "email" },
+      ].map(({ name, slug, icon }) => (
+        <li key={`orkut__icon_set__${slug}`}>
+          <span
+            style={{ gridArea: "title" }}
+            className="OrkutNostalgicIconSet__title"
+          >
+            {name}
+          </span>
+          <span
+            className="OrkutNostalgicIconSet__number"
+            style={{ gridArea: "number" }}
+          >
+            <img
+              key={`orkut__icon_set__${slug}_img`}
+              className="OrkutNostalgicIconSet__iconSample"
+              src={`icons/${icon}.svg`}
+            />
+            {props[slug] ? props[slug] : 0}
+          </span>
+        </li>
+      ))}
+      {[
+        { name: "Confiável", slug: "confiavel", icon: "smile" },
+        { name: "Legal", slug: "legal", icon: "cool" },
+        { name: "Sexy", slug: "sexy", icon: "heart" },
+      ].map(({ name, slug, icon }) => {
+        const total = props[slug] ? props[slug] : 2
+        return (
+          <li key={`orkut__icon_set__${slug}`}>
+            <span className="OrkutNostalgicIconSet__title">{name}</span>
+            <span
+              className="OrkutNostalgicIconSet__iconComplex OrkutNostalgicIconSet__number"
+              style={{ gridArea: "number" }}
+            >
+              {[0, 1, 2].map((_, index) => {
+                const isHeartActive = index <= total - 1
+                return (
+                  <img
+                    key={`orkut__icon_set__${slug}_img_${index}`}
+                    src={`icons/${icon}.svg`}
+                    style={{
+                      marginRight: "2px",
+                      opacity: isHeartActive ? 1 : "0.5",
+                    }}
+                  />
+                )
+              })}
+            </span>
+          </li>
+        )
+      })}
+    </OrkutNostalgicIconSet.List>
+  )
+}
+OrkutNostalgicIconSet.List = styled.ul`
+  margin-top: 32px;
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  li {
+    font-size: 12px;
+    color: #5a5a5a;
+    display: grid;
+    grid-template-areas:
+      "title title"
+      "number number";
+
+    &:not(:last-child) {
+      margin-right: 5px;
+    }
+    .OrkutNostalgicIconSet__title {
+      display: block;
+      font-style: italic;
+    }
+    .OrkutNostalgicIconSet__number {
+      min-width: 15px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      .OrkutNostalgicIconSet__iconSample {
+        margin-right: 7px;
+      }
+    }
+  }
+`
+
+// ================================================================================================================
+// Login Page
+// ================================================================================================================
+const GitkutLoginScreen = css`
+  :root {
+    --backgroundPrimary: #d9e6f6;
+    --backgroundSecondary: #f1f9fe;
+    --backgroundTertiary: #ffffff;
+    --backgroundQuarternary: #bbcde8;
+    --colorPrimary: #2e7bb4;
+    --colorSecondary: #388bb0;
+    --colorTertiary: #2f4a71;
+    --colorQuarternary: #d81d99;
+    --textPrimaryColor: #333333;
+    --textSecondaryColor: #ffffff;
+    --textTertiaryColor: #5a5a5a;
+    --textQuarternaryColor: #c5c6ca;
+    --commonRadius: 8px;
+  }
+
+  .loginScreen {
+    padding: 16px;
+    max-width: 1110px;
+    display: grid;
+    --gap: 12px;
+    --gutter: 16px;
+    grid-gap: var(--gap);
+    grid-template-areas:
+      "logoArea"
+      "formArea"
+      "footerArea";
+    @media (min-width: 860px) {
+      grid-template-columns: 2fr 1fr;
+      grid-template-areas:
+        "logoArea formArea"
+        "logoArea formArea"
+        "footerArea footerArea";
+    }
+    .logoArea {
+      grid-area: logoArea;
+      background-color: var(--backgroundTertiary);
+      border-radius: var(--commonRadius);
+      padding: var(--gutter);
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: center;
+      min-height: 263px;
+      @media (min-width: 860px) {
+        min-height: 368px;
+      }
+      p {
+        font-size: 12px;
+        line-height: 1.2;
+        &:not(:last-child) {
+          margin-bottom: 12px;
+        }
+        strong {
+          color: var(--colorQuarternary);
+        }
+      }
+      img {
+        max-height: 45px;
+        margin-bottom: 36px;
+      }
+    }
+    .formArea {
+      grid-area: formArea;
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: column;
+      .box {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        padding: var(--gutter);
+        padding-left: 50px;
+        padding-right: 50px;
+        background-color: var(--backgroundSecondary);
+        border-radius: var(--commonRadius);
+        flex: 1;
+        &:not(:last-child) {
+          margin-bottom: var(--gap);
+        }
+        &:first-child {
+          min-height: 224px;
+          @media (min-width: 860px) {
+            min-height: 282px;
+          }
+        }
+        p {
+          font-size: 14px;
+        }
+        a {
+          text-decoration: none;
+          color: var(--colorPrimary);
+        }
+        input {
+          width: 100%;
+          display: block;
+          border: 1px solid var(--textQuarternaryColor);
+          padding: 12px;
+          background-color: var(--backgroundTertiary);
+          border-radius: var(--commonRadius);
+          margin-top: 24px;
+          margin-bottom: 16px;
+        }
+        button {
+          width: 100%;
+          display: block;
+          border: 0;
+          padding: 12px;
+          border-radius: var(--commonRadius);
+          background-color: var(--colorPrimary);
+          color: var(--textSecondaryColor);
+        }
+      }
+    }
+    .footerArea {
+      grid-area: footerArea;
+      background-color: var(--backgroundQuarternary);
+      border-radius: var(--commonRadius);
+      padding: 8px;
+      p {
+        font-size: 12px;
+        text-align: center;
+        a {
+          text-decoration: none;
+          color: var(--colorPrimary);
+        }
+      }
+    }
+  }
+`
+
+// ================================================================================================================
+// Reset Styles
+// ================================================================================================================
+export const GitkutStyles = css`
+  *::-webkit-scrollbar {
+    width: 8px;
+  }
+  *::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+  *::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 10px;
+  }
+  *::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+  a,
+  button {
+    cursor: pointer;
+    transition: 0.3s;
+    outline: 0;
+    &:hover,
+    &:focus {
+      opacity: 0.8;
+    }
+    &:disabled {
+      cursor: not-allowed;
+      opacity: 0.5;
+    }
+  }
+  input {
+    transition: 0.3s;
+    outline: 0;
+    &:disabled {
+      cursor: not-allowed;
+      opacity: 0.5;
+    }
+    &:hover,
+    &:focus {
+      box-shadow: 0px 0px 5px #33333357;
+    }
+  }
+
+  ${GitkutLoginScreen}
+`
